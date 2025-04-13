a99c41f91a8d3a59aa0572cb2d4997f7be7cc0c2
Sudip Sharma <sudpokhara826@gmail.com>
2025-04-13 16:16:42 +0545

    Improved Banner design and make the ux better

--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -4,7 +4,7 @@ import { ArrowDown, ShoppingBag } from 'lucide-react';
 
 const MainBanner = () => {
   return (
-    <div className='relative w-full h-[300px] md:h-[400px] lg:h-[500px] overflow-hidden rounded-2xl shadow-xl'>
+    <div className='relative w-full h-[300px] md:h-[400px] lg:h-[420px] overflow-hidden rounded-4xl shadow-xl'>
       {/* Background Image */}
       <img 
         src="/assets/main_banner_bg.png" 
@@ -21,11 +21,11 @@ const MainBanner = () => {
       <div className="absolute inset-0 bg-gradient-to-r from-black/80 via-black/60 to-black/40"></div>
 
       {/* Content */}
-      <div className='absolute inset-0 flex flex-col justify-center items-center px-6 md:px-16'>
+      <div className='absolute inset-0 flex flex-col justify-center items-center px-4 md:px-16'>
         <div className='max-w-4xl mx-auto text-center relative z-10'>
           <h1 className='text-3xl md:text-5xl lg:text-6xl font-bold text-white mb-6 tracking-tight leading-tight'>
             Welcome to{' '}
-            <span className='text-red-500 animate-pulse'>Kirana</span>{' '}
+            <span className='text-yellow-400 animate-pulse'>Kirana</span>{' '}
             <span className='text-blue-400'>Nepal</span>
           </h1>
           <p className='text-lg md:text-xl lg:text-2xl text-gray-200 mb-12 max-w-2xl mx-auto leading-relaxed'>
@@ -40,10 +40,11 @@ const MainBanner = () => {
             <span>Shop Now</span>
             <ShoppingBag className="w-5 h-5 group-hover:rotate-12 transition-transform" />
           </Link>
-          <div className='absolute -bottom-14 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce'>
-            <span className='text-white text-sm'>Scroll Down</span>
-            <ArrowDown className='w-6 h-6 text-white opacity-75 animate-bounce' />
-            
+          <div className='absolute -bottom-15 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce'>
+          <Link to={"/deals"} className='group hidden md:flex items-center gap-2 px-9 py-2 cursor-pointer'>
+          Explore deals
+            <ArrowDown className='w-6 h-6 text-white opacity-75' />
+          </Link>
           </div>
         </div>
       </div>
